bbd22c534f47493018cb46bf4c42d8c86094b420
jpcossi <jppcossi@gmail.com>
2024-08-07 19:43:17 -0300

    add new routes, css, views, handlebars.

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-const express = require('express')
+const express    = require('express')
+const exphbs     = require('express-handlebars')
+const path       = require('path')
+const db         = require('./db/connection')
 const bodyParser = require('body-parser')
-const db = require('./db/connection.js')
+const Job        = require('./models/Job')
+const Sequelize  = require('sequelize')
+const Op         = Sequelize.Op
 
 const app = express()
 const PORT = 3000
@@ -11,6 +16,14 @@ app.listen (PORT, function(){
 
 /// body parser
 app.use(bodyParser.urlencoded({ extended: false}))
+
+// handle bars
+app.set('views', path.join(__dirname, 'views'))
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+//static folder
+app.use(express.static(path.join(__dirname, 'public')))
 
 //db connection
 db
@@ -23,7 +36,38 @@ db
 
 //routes
 app.get('/', (req, res) =>{
-  res.send("Esta funcionando 2")
+  let search = req.query.job;
+  let query  = '%'+search+'%'; // PH -> PHP, Word -> Wordpress, press -> Wordpress
+
+  if(!search) {
+    Job.findAll({order: [
+      ['createdAt', 'DESC']
+    ]})
+    .then(jobs => {
+  
+      res.render('index', {
+        jobs
+      });
+  
+    })
+    .catch(err => console.log(err));
+  } else {
+    Job.findAll({
+      where: {title: {[Op.like]: query}},
+      order: [
+        ['createdAt', 'DESC']
+    ]})
+    .then(jobs => {
+      console.log(search);
+      console.log(search);
+  
+      res.render('index', {
+        jobs, search
+      });
+  
+    })
+    .catch(err => console.log(err));
+  }
 })
 
 //routes jobs
--- routes/jobs.js
+++ routes/jobs.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const router = express.Router()
-const Job = require('../models/job')
+const Job = require('../models/Job.js')
 
-router.get('/teste', (req, res) => {
-  res.send("get deu certo")
+router.get('/add', (req, res) => {
+  res.render('add')
 })
+
+router.get('/view/:id', (req, res) => Job.findOne({
+  where: {id: req.params.id}
+}).then(job => {
+    res.render('view', {
+      job
+    })
+  }).catch(err => console.log(err))
+)
 
 router.post('/add', (req, res) =>{
   const {title, description, email, salary, company, new_job} = req.body
